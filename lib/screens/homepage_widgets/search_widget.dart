6c4f195cf87cc2d1d071a70c81533f3cae136bb1
Phan Thành Ngân <nngann2402@gmail.com>
2024-05-05 20:57:35 +0700

    Design Template HomePage

---
+++ lib/screens/homepage_widgets/search_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatefulWidget {
+  const SearchWidget({super.key});
+
+  @override
+  State<SearchWidget> createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search'),
+      ),
+      body: const Center(
+        child: Text('Search'),
+      ),
+    );
+  }
+}
